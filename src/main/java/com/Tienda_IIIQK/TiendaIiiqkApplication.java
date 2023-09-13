02aeeebe69669cbedda9f74764ba41acfaf6c38a
josep <josep@JosePadilla.lan>
2023-09-13 09:44:59 -0600

    creacion proyecto inicial

---
+++ src/main/java/com/Tienda_IIIQK/TiendaIiiqkApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_IIIQK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiiqkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiiqkApplication.class, args);
+	}
+
+}
