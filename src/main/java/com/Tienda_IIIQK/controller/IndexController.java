058e6d7e2379dfa0887159157940eb985985a674
josep <josep@JosePadilla>
2023-12-05 08:45:21 -0600

    semana 12 Jose Alberto Padill Rivas, items y reportes

--- src/main/java/com/Tienda_IIIQK/controller/IndexController.java
+++ src/main/java/com/Tienda_IIIQK/controller/IndexController.java
@@ -1,17 +1,43 @@
-
 package com.Tienda_IIIQK.controller;
 
+import com.Tienda_IIIQK.domain.Item;
+import com.Tienda_IIIQK.service.ItemService;
+import com.Tienda_IIIQK.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-    
+
+    @Autowired
+    ProductoService productoService;
+
+    @Autowired
+    private ItemService itemService;
+
     @RequestMapping("/")
     public String page(Model model) {
-        model.addAttribute("attribute", "value");
+        var listaProductos = productoService.getProductos(true);
+        model.addAttribute("productos", listaProductos);
         return "index";
     }
     
+    @RequestMapping("/refrescarBoton")
+    public ModelAndView refrescarBoton(Model model){
+        var lista = itemService.gets();
+        var totalCarritos = 0;
+        var carritoTotalVenta = 0;
+        for (Item i : lista) {
+            totalCarritos += i.getCantidad();
+            carritoTotalVenta += (i.getCantidad() * i.getPrecio());
+        }
+        model.addAttribute("listaItems", lista);
+        model.addAttribute("listaTotal", totalCarritos);
+        model.addAttribute("carritoTotal", carritoTotalVenta);
+        return new ModelAndView("/carrito/fragmentos :: verCarrito");
+    }
+
 }
